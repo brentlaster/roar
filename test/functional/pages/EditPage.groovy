d2a8ddf8649bdad702f6adcacbeab9fe4dbb296a
Brent Laster <Brent.Laster@sas.com>
2012-11-12 21:17:29 -0500

    Update template EditPage.groovy

--- test/functional/pages/EditPage.groovy
+++ test/functional/pages/EditPage.groovy
@@ -3,12 +3,11 @@ package pages
 class EditPage extends ScaffoldPage {
 
 	static at = {
-		heading.text() ==~ /Edit.+/
+		
 	}
 	
 	static content = {
-		updateButton(to: ShowPage) { $("input", value: "Update") }
-		deleteButton(to: ListPage) { $("input", value: "Delete") }
+		
 	}
 
 }
